a4e79649383815c740957c19be9cf750f2f01cad
Antonio Mancera Gamez <amanceragamez@gmail.com>
2024-01-07 17:57:58 +0100

    Organized to UI

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,23 +1,49 @@
 local map = vim.keymap.set
 
-map('i', 'jk', '<ESC>')
-map('n', '<LEADER>w', ':w<CR>')
-map('n', '<LEADER>q', ':q<CR>')
+-- QOL keymaps
+--map('i', 'jk', '<ESC>')
+--map('n', '<LEADER>w', ':w<CR>')
+--map('n', '<LEADER>q', ':q<CR>')
 map('n', '<ESC>', ':noh<CR>')
-map('n', "<LEADER>fq", ":q!<CR>")
+--map('n', "<LEADER>fq", ":q!<CR>")
 
+
+-- Open telescope
+local tlsc = require('telescope.builtin')
+map('n', '<LEADER>nn', "<CMD> Neotree action=focus source=filesystem position=current reveal=true <CR>")
+map('n', '<LEADER>ng', "<CDM> Neotree action=focus source=git_status position=current reveal=true<CR>")
+map('n', '<LEADER>ff', tlsc.find_files, {})
+map('n', '<LEADER>fb', tlsc.buffers, {})
+map('n', '<LEADER>fg', tlsc.live_grep, {})
+
+-- Move around buffers
+map('n', '<C-b>n', ':bnext<cr>')
+map('n', '<C-b>p', ':bprevious<cr>')
+map('n', '<C-b>d', ':bdelete<cr>')
 map('n', '<C-h>', '<C-w>h')
 map('n', '<C-j>', '<C-w>j')
 map('n', '<C-k>', '<C-w>k')
 map('n', '<C-l>', '<C-w>l')
 
-local tlsc = require('telescope.builtin')
-vim.keymap.set('n', '<LEADER>nn', "<CMD> Neotree action=focus source=filesystem position=current reveal=true <CR>")
-vim.keymap.set('n', '<LEADER>ng', "<CDM> Neotree action=focus source=git_status position=current reveal=true<CR>")
-vim.keymap.set('n', '<LEADER>ff', tlsc.find_files, {})
-vim.keymap.set('n', '<LEADER>fb', tlsc.buffers, {})
-vim.keymap.set('n', '<LEADER>fg', tlsc.live_grep, {})
+-- LSP actions
+vim.api.nvim_create_autocmd('LspAttach', {
+  desc = 'LSP actions',
+  callback = function(event)
+    local opts = {buffer = event.buf}
 
-vim.keymap.set('n', '<C-b>n', ':bnext<cr>')
-vim.keymap.set('n', '<C-b>p', ':bprevious<cr>')
-vim.keymap.set('n', '<C-b>d', ':bdelete<cr>')
+    map('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
+    map('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+    map('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
+    map('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
+    map('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
+    map('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
+    map('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
+    map('n', ',rn', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
+    map({'n', 'x'}, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
+    map('n', ',ca', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+
+    map('n', ',e', '<cmd>lua vim.diagnostic.open_float()<cr>', opts)
+    map('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<cr>', opts)
+    map('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<cr>', opts)
+  end
+})
--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -6,65 +6,8 @@ return {
       "nvim-lua/plenary.nvim",
       "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
       "MunifTanjim/nui.nvim",
-      "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
     }
   },
-  {
-    'nvim-lualine/lualine.nvim',
-    dependencies = {
-      'nvim-tree/nvim-web-devicons'
-    },
-    config = function()
-      require('lualine').setup {
-        options = {
-          icons_enabled = true,
-          theme = 'auto',
-          component_separators = { left = '', right = ''},
-          section_separators = { left = '', right = ''},
-          disabled_filetypes = {
-            statusline = {"alpha"},
-            winbar = {"alpha"},
-          },
-          ignore_focus = {},
-          always_divide_middle = true,
-          globalstatus = false,
-          refresh = {
-            statusline = 1000,
-            tabline = 1000,
-            winbar = 1000,
-          }
-        },
-        sections = {
-          lualine_a = {'mode'},
-          lualine_b = {'branch', 'diff', 'diagnostics'},
-          lualine_c = {'filename'},
-          lualine_x = {'encoding', 'fileformat', 'filetype'},
-          lualine_y = {'progress'},
-          lualine_z = {'location'}
-        },
-        inactive_sections = {
-          lualine_a = {},
-          lualine_b = {},
-          lualine_c = {'filename'},
-          lualine_x = {'location'},
-          lualine_y = {},
-          lualine_z = {}
-        },
-        tabline = {
-          lualine_a = {'buffers'}
-        },
-        winbar = {},
-        inactive_winbar = {},
-        extensions = {}
-      }
-    end,
-  },
-  {
-
-  },
-  { "alvan/vim-closetag" },
-  { "tpope/vim-repeat" },
-  { "sheerun/vim-polyglot", enabled = false},
   {
     'echasnovski/mini.nvim',
     version = false,
@@ -84,24 +27,9 @@ return {
       require("mini.operators").setup()
       require("mini.animate").setup()
     end
-  },
-  {
-    'norcalli/nvim-colorizer.lua',
-    config = function ()
-      require('colorizer').setup()
-    end
-  },
-  {
+  }, {
     "nvim-telescope/telescope.nvim",
     version = "*",
     dependencies = { "nvim-lua/plenary.nvim" }
-  },
-  {
-    "lukas-reineke/indent-blankline.nvim",
-    main = "ibl",
-    opts = {},
-    config = function ()
-      require('ibl').setup()
-    end
   }
 }
